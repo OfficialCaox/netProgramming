51775d0cfca158e5c34036549f39271a22fda4b6
OfficialCaox <125695165+OfficialCaox@users.noreply.github.com>
2023-03-08 03:38:27 +0900

    Update MainProgram
    write line

--- netProgramming/Program.cs
+++ netProgramming/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Internal;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+            Console.WriteLine("Write");
+        }
+    }
+}
